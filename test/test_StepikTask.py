87697f644db8fcd77fdd1f3825882834b6ab5ab6
Roman Kalganov <roman_kalganov@epam.com>
2022-11-19 13:39:16 +0300

    mocks with function

--- test/test_StepikTask.py
+++ test/test_StepikTask.py
@@ -8,12 +8,20 @@ from main.Converter import converter
 from main.StepikTask import StepikTask
 
 
+def mock_input_response(*args, **kwargs):
+    # if not args[0] == BinaryFraction("0", "0", "0"):
+    #     raise ValueError()
+    return Fraction(4, 7)
+
+
 class TestStepikTask(TestCase):
 
     def test_solve_task(self):
         mock_builtin_function = mock.patch('builtins.input', side_effect=["0.0(101)", "101"])
+        # mock_method_on_object = mock.patch.object(converter, "to_decimal_fraction",
+        #                                           side_effect=[Fraction(4, 7), Fraction(5, 1)])
         mock_method_on_object = mock.patch.object(converter, "to_decimal_fraction",
-                                                  side_effect=[Fraction(4, 7), Fraction(5, 1)])
+                                                  side_effect=mock_input_response)
         mock_static_method = mock.patch.object(BinaryFractionParser, "parse_binary_fraction",
                                                side_effect=[BinaryFraction("0", "101", "0"),
                                                             BinaryFraction("101", None, None)])
